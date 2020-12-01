5028718240c8925f79438d153d9f883c508608da
vovakms <vova-game@xaker.ru>
2020-12-01 20:03:06 +1000

    доб зад
    доб зад

---
+++ ЗадачаЧислоПалиндром/ЗадачаЧислоПалиндром/MainModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЗадачаЧислоПалиндром
+{
+    public class MainModel
+    {
+        public static string ReverseStr(string intput)
+        {
+            if (intput == null) intput = "";
+             
+            string revers = "";
+            
+            for (int i = intput.Length - 1; i >= 0; i--)
+            {
+                revers += intput[i] ;
+            }
+            
+            return revers;
+        }
+
+        public static string CheckOK(string intput, string revers)
+        {
+           return  intput == revers ? "ДА - это палиндром" : "НЕТ - введенное число НЕ палиндром";
+        }
+
+    }
+}
---
+++ ЗадачаЧислоПалиндром/ЗадачаЧислоПалиндром/MainVM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ЗадачаЧислоПалиндром
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+       
+        private string var01;
+        
+        public string Var1
+        {
+            get { return var01; }
+            set
+            {
+                var01 = value;
+                OnPropertyChanged("Var2");
+                OnPropertyChanged("Var3");
+            }
+        }
+         
+        public string Var2
+        {
+            get { return MainModel.ReverseStr(Var1); }
+        }
+
+        public string Var3
+        {
+            get { return MainModel.CheckOK(Var1, Var2); }
+        }
+  
+       
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
